c9490feec628ba63a1613d6c895b87e9b7f74cb7
Javier Repi <xavirepi@hotmail.com>
2021-03-09 18:26:14 +0100

    work on offer responsiveness

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -17,6 +17,27 @@ setTimeout(() => {
 
 
 // OFFERS LIST
+window.onscroll = (ev) => {
+  if(window.innerHeight + window.scrollY >=  document.getElementById('offer-list-navbar').scrollHeight) {
+    document.getElementById('offer-list-navbar').classList.add('fixed-top');
+    document.getElementById('navbar').classList.add('fixed-top');
+  } else if (window.innerHeight + window.scrollY >=  document.getElementById('offer-list-navbar').scrollHeight){
+    document.getElementById('offer-list-navbar').classList.add('fixed-top');
+    document.getElementById('navbar').classList.add('fixed-top');
+  }
+}
+
 document.getElementById('show-filters').addEventListener('click', () => {
-  document.getElementById('filters').classList.toggle('d-none')
+  document.getElementById('filters').classList.toggle('d-none');
+  document.getElementById('offer-list').classList.toggle('d-none');
 })
+
+document.getElementById('show-detail').addEventListener('click', () => {
+  document.getElementById('detail').classList.toggle('d-none');
+})
+
+// document.querySelectorAll('show-detail').forEach(link => { 
+//   link.addEventListener('click', () => {
+//   document.getElementById('detail').classList.toggle('d-none');
+//   })
+// }
