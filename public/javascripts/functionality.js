ad40f589f01e621504b7922c7905a5fde368417d
Ángela Herrador <angelaherrador1991@gmail.com>
2021-03-09 22:08:36 +0100

    work on candidates and companies profiles

--- public/javascripts/functionality.js
+++ public/javascripts/functionality.js
@@ -25,5 +25,13 @@ document.onscroll = () => {
   }
 }
 
-//SIGNIN
-document.getElementById('candidate-forgot-password').addEventListener('click', ())
+//SIGNIN/ LOGIN
+document.getElementById('candidate-forgot-password').addEventListener('click', () => {
+  document.getElementById('restoreEmail').classList.toggle('d-none')
+})
+
+document.getElementById('privacity').addEventListener('click', () => {
+  document.getElementById('dropOut').classList.toggle('d-none')
+  document.getElementById('restorePass').classList.toggle('d-none')
+  document.getElementById('restoreEm').classList.toggle('d-none')
+})
