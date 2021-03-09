c82ff76c78b44b12b2dece62a00615f706b5fbf7
Javier Repi <xavirepi@hotmail.com>
2021-03-09 21:12:13 +0100

    correct code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,16 +18,12 @@ setTimeout(() => {
 
 // OFFERS LIST
 window.onscroll = (ev) => {
-  console.log('window.scrollY', window.scrollY)
-  console.log('document.body.scrollTop', window.innerHeight)
-
   if (window.scrollY > 65 && document.body.scrollTop <= 0) {
     document.getElementById('offer-list-navbar').classList.add('fixed-top');
     document.getElementById('offer-list-navbar').classList.add('bg-white');
   } else if (window.scrollY < 30) {
     document.getElementById('offer-list-navbar').classList.remove('fixed-top');
-    //document.getElementById('navbar').classList.remove('d-none');
-    // document.getElementById('navbar').classList.add('fixed-top');
+    document.getElementById('offer-list-navbar').classList.remove('bg-white');
   }
 }
 
@@ -36,17 +32,11 @@ let showFilters = document.getElementById('show-filters');
 showFilters.addEventListener('click', () => {
   document.getElementById('offer-list').classList.toggle('d-none');
   document.getElementById('filters').classList.toggle('d-none');
-  
-  // if (window.scrollY > 65) {
-  //   document.getElementById('navbar').classList.add('d-none');
-  // } else if (window.scrollY > 65) {
-  //   document.getElementById('navbar').classList.remove('d-none');
-  // }
 
   if (showFilters.innerHTML === 'FILTROS') {
     showFilters.innerHTML ='OCULTAR'
-    showFilters.classList.remove('btn-primary')
-    showFilters.classList.add('btn-outline-success')
+    showFilters.classList.remove('btn-outline-primary')
+    showFilters.classList.add('btn-success')
   } else {
     showFilters.innerHTML = 'FILTROS'
     showFilters.classList.remove('btn-success')
@@ -54,12 +44,6 @@ showFilters.addEventListener('click', () => {
   }
 })
 
-document.getElementById('show-detail').addEventListener('click', () => {
+document.querySelectorAll('show-detail').addEventListener('click', () => {
   document.getElementById('detail').classList.toggle('d-none');
 })
-
-// document.querySelectorAll('show-detail').forEach(link => { 
-//   link.addEventListener('click', () => {
-//   document.getElementById('detail').classList.toggle('d-none');
-//   })
-// }
