5c1833fd8d1943d114e98de9f46ad49b8a3b2e7d
Ángela Herrador <angelaherrador1991@gmail.com>
2021-03-09 20:46:53 +0100

    Work on design

--- public/javascripts/functionality.js
+++ public/javascripts/functionality.js
@@ -1,8 +1,3 @@
-// NAVBAR
-document.getElementById('show-login').addEventListener('click', () => {
-    document.getElementById('main-login').classList.toggle('d-none');
-}) 
-
 // HOME PAGE
 document.getElementById('show-employment').addEventListener('click', () => {
     document.getElementById('employment').classList.remove('d-none');
@@ -13,3 +8,22 @@ document.getElementById('show-skills').addEventListener('click', () => {
     document.getElementById('skills').classList.remove('d-none');
     document.getElementById('employment').classList.add('d-none');
 }) 
+
+//NAVBAR PARALLAX
+let depth
+
+document.onscroll = () => {
+
+  depth = window.scrollY
+
+  //console.log(depth)
+  
+  if (depth > 100) {
+    document.querySelector('.navbar').classList.add('thin')  
+  } else {
+    document.querySelector('.navbar').classList.remove('thin')
+  }
+}
+
+//SIGNIN
+document.getElementById('candidate-forgot-password').addEventListener('click', ())
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,7 +7,7 @@ function update(input) {
   return confirm(`¿Seguro que quieres actualizar?`);
 }
   
-FLASH
+// FLASH
 setTimeout(() => {
   document.querySelectorAll('.toast').forEach(toast => {
     console.log(toast)
@@ -32,20 +32,3 @@ setTimeout(() => {
 //   axios.get(`http://localhost:3000/offers-list?page=${loadMore}`)
 //   .then((response) => console.log(response))
 // })
-
-
-//NAVBAR PARALLAX
-let depth
-
-document.onscroll = () => {
-
-  depth = window.scrollY
-
-  //console.log(depth)
-  
-  if (depth > 100) {
-    document.querySelector('.navbar').classList.add('thin')  
-  } else {
-    document.querySelector('.navbar').classList.remove('thin')
-  }
-}
