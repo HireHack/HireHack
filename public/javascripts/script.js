4d575ba34eb3ac9aed7f0ce39534f62f73199a3f
Javier Repi <xavirepi@hotmail.com>
2021-03-09 20:47:05 +0100

    finish responsive design offer view

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,18 +18,40 @@ setTimeout(() => {
 
 // OFFERS LIST
 window.onscroll = (ev) => {
-  if(window.innerHeight + window.scrollY >=  document.getElementById('offer-list-navbar').scrollHeight) {
+  console.log('window.scrollY', window.scrollY)
+  console.log('document.body.scrollTop', window.innerHeight)
+
+  if (window.scrollY > 65 && document.body.scrollTop <= 0) {
     document.getElementById('offer-list-navbar').classList.add('fixed-top');
-    document.getElementById('navbar').classList.add('fixed-top');
-  } else if (window.innerHeight + window.scrollY >=  document.getElementById('offer-list-navbar').scrollHeight){
-    document.getElementById('offer-list-navbar').classList.add('fixed-top');
-    document.getElementById('navbar').classList.add('fixed-top');
+    document.getElementById('offer-list-navbar').classList.add('bg-white');
+  } else if (window.scrollY < 30) {
+    document.getElementById('offer-list-navbar').classList.remove('fixed-top');
+    //document.getElementById('navbar').classList.remove('d-none');
+    // document.getElementById('navbar').classList.add('fixed-top');
   }
 }
 
-document.getElementById('show-filters').addEventListener('click', () => {
-  document.getElementById('filters').classList.toggle('d-none');
+let showFilters = document.getElementById('show-filters');
+
+showFilters.addEventListener('click', () => {
   document.getElementById('offer-list').classList.toggle('d-none');
+  document.getElementById('filters').classList.toggle('d-none');
+  
+  // if (window.scrollY > 65) {
+  //   document.getElementById('navbar').classList.add('d-none');
+  // } else if (window.scrollY > 65) {
+  //   document.getElementById('navbar').classList.remove('d-none');
+  // }
+
+  if (showFilters.innerHTML === 'FILTROS') {
+    showFilters.innerHTML ='OCULTAR'
+    showFilters.classList.remove('btn-primary')
+    showFilters.classList.add('btn-outline-success')
+  } else {
+    showFilters.innerHTML = 'FILTROS'
+    showFilters.classList.remove('btn-success')
+    showFilters.classList.add('btn-outline-primary')
+  }
 })
 
 document.getElementById('show-detail').addEventListener('click', () => {
